b27df568d5b450bc7dc85e698fe544281a17dfac
andrusarches <64173881+andrusarches@users.noreply.github.com>
2021-07-02 19:53:52 +0300

    module1-task1

---
+++ src/frontend/src/common/const.js
@@ -0,0 +1,33 @@
+export const PIZZA_MULTIPLIER_TO_SIZE = {
+  1: "small",
+  2: "normal",
+  3: "big",
+};
+
+export const SAUCE_NAME_TO_VALUE = {
+  Томатный: "tomato",
+  Сливочный: "creamy",
+};
+
+export const INGREDIENTS_ENGLISH = {
+  Грибы: "mushrooms",
+  Чеддер: "cheddar",
+  Салями: "salami",
+  Ветчина: "ham",
+  Ананас: "ananas",
+  Бекон: "bacon",
+  Лук: "onion",
+  Чили: "chile",
+  Халапеньо: "jalapeno",
+  Маслины: "olives",
+  Томаты: "tomatoes",
+  Лосось: "salmon",
+  Моцарелла: "mozzarella",
+  Пармезан: "parmesan",
+  "Блю чиз": "blue_cheese",
+};
+
+export const DOUGH_ENGLISH = {
+  Тонкое: "light",
+  Толстое: "large",
+};
